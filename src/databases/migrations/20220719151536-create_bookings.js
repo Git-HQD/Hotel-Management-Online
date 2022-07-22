8b6545cf2cfa8a23954f73c207bf716bb86a9c67
KaiDioz <phanqthientan@gmail.com>
2022-07-22 18:20:25 +0700

    Fix comment

--- src/databases/migrations/20220719151536-create_bookings.js
+++ src/databases/migrations/20220719151536-create_bookings.js
@@ -1,33 +1,36 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('bookings',{
-     id:{
-      allowNull: false,
-      primaryKey:true,
-      autoIncrement: true,
-      type: Sequelize.DataTypes.INTEGER
-     },
-     user_id:{
-      type: Sequelize.DataTypes.INTEGER,
-      references: {
-        model: {
-          tableName: 'users',
-        },
-        key: 'id'
+    await queryInterface.createTable('bookings', {
+      id: {
+        allowNull: false,
+        primaryKey:true,
+        autoIncrement: true,
+        type: Sequelize.DataTypes.INTEGER
       },
-     },
-     price:{
-      type: Sequelize.DataTypes.DECIMAL
-     },
-     check_in:{
-      type: Sequelize.DataTypes.DATE
-     },
-     check_out:{
-      type: Sequelize.DataTypes.DATE
-     },
-     booking_date:{
-      type: Sequelize.DataTypes.DATE
-     }
+      user_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        references: {
+          model: {
+            tableName: 'users',
+          },
+          key: 'id'
+        },
+      },
+      price: {
+        type: Sequelize.DataTypes.DECIMAL
+      },
+      check_in: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: false
+      },
+      check_out: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: false
+      },
+      booking_date: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: false
+      }
     })
   },
 
